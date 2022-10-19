47e9a73903eeeb41c21dd2891de97f7698ac40b0
your username <rodrigodjr1@gmail.com>
2022-10-18 21:21:55 -0300

    manipulando json

--- app.py
+++ app.py
@@ -3,26 +3,54 @@ import json
 
 app = Flask(__name__)
 
-pronto = {}
+id = 0
 
 @app.route("/")
-def index():
-
-
-
+def index():             
     return render_template('index.html')
 
+@app.route("/cards", methods=["GET"])
+def cards():
+    usuario = request.args.get('username')
+    print(usuario)
+    file = open('dados.json')
+    dados = json.load(file)
+
+    for user in dados:
+        if user["usuario"] == usuario:
+            print('foi')
+            id = user["id"]
+            print(user["usuario"])
+            return render_template('cartas.html')
+        else:
+            return render_template('index.html')
+    return 'logado'
 
 @app.route("/pay", methods=['GET'])
 def pagamento():
-    file = open('dados.json')
-    dados = json.load(file)
+    file = open('cartas.json')
+    card = json.load(file)
+    file2 = open('dados.json')
+    users = json.load(file2)
        
-    id = request.args.get('id')
+    idCarta = request.args.get('id')
     
+    for carta in card:
+        if carta['id'] == idCarta:
+            valor = carta['valor']
+            for user in users:
+                if user["id"] == id:
+                    carteira = user["carteira"]
+                    print(carteira)
+                    carteira -= valor
+    file.close()
+    file2.close()
+    users.append({
+        "carteira": users["carteira"],
+    })
 
     print(id)
 
     with open('dados.json','w') as arq:
-        arq.write(json.dumps(pronto))
+        arq.write(json.dumps(user))
     return 'i'
